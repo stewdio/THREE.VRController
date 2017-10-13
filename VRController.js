3f54c2101e8d9c42baee145c61b4088fdc572fa3
Stewart <stew@rtsmith.io>
2017-10-13 13:58:43 -0400

    Refined comments on functionality.

--- VRController.js
+++ VRController.js
@@ -664,8 +664,9 @@ THREE.VRController.supported = {
 		style: 'daydream',
 
 
-		//  Daydream’s thumbpad is both a 2D trackpad and a button with both
-		//  touch and press. The Y-axis is Regular.
+		//  THUMBPAD
+		//  Both a 2D trackpad and a button with both touch and press. 
+		//  The Y-axis is “Regular”.
 		//
 		//              Top: Y = -1
 		//                   ↑
@@ -693,8 +694,9 @@ THREE.VRController.supported = {
 		style: 'vive',
 
 
-		//  Vive’s thumbpad is both a 2D trackpad and a button. Its Y-axis is 
-		//  Goofy -- in contrast to Daydream, Oculus, Microsoft, etc.
+		//  THUMBPAD
+		//  Both a 2D trackpad and a button. Its Y-axis is “Goofy” --
+		//  in contrast to Daydream, Oculus, Microsoft, etc.
 		//
 		//              Top: Y = +1
 		//                   ↑
@@ -706,27 +708,29 @@ THREE.VRController.supported = {
 		buttons: [
 
 
-			//  Vive’s thumbpad registers touch and press distinctly.
+			//  THUMBPAD
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
-			//  isTouched: ACTUAL BOOLEAN FOR TOUCH!
+			//  isTouched: YES has real touch detection.
 			//  isPressed: As expected.
 
 			'thumbpad',
 
 
-			//  ===  Chromium  ===============================================
+			//  TRIGGER
+			//  Has very interesting and distinct behavior on Chromium.
+			//  The threshold for releasing a pressed state is higher during
+			//  engagement and lower during release.
+			//
+			//  Chromium
 			//  if( value >  0.00 ) isTouched = true else isTouched = false
-			//  UPON ENGAGING
-			//  if( value >= 0.55 ) isPressed = true
-			//  UPON RELEASING
-			//  if( value <  0.45 ) isPressed = false
-
-			//  ===  Firefox  ================================================
-			//  UPON ENGAGING
+			//  if( value >= 0.55 ) isPressed = true   UPON ENGAGING
+			//  if( value <  0.45 ) isPressed = false  UPON RELEASING
+			//
+			//  Firefox
 			//  if( value >= 0.10 ) isTouched = isPressed = true
-			//  UPON RELEASING
 			//  if( value <  0.10 ) isTouched = isPressed = false
-
+			//  --------------------------------------------------------------
 			//  value:     Analog 0 to 1.
 			//  isTouched: Duplicates isPressed in FF, independent in Chrome.
 			//  isPressed: Corresponds to value.
@@ -734,13 +738,12 @@ THREE.VRController.supported = {
 			'trigger',
 
 
+			//  GRIPS
 			//  Each Vive controller has two grip buttons, one on the left and
 			//  one on the right. They are not distinguishable -- pressing 
 			//  either one will register as a press with no knowledge of which
-			//  one was pressed. This value is binary, 0 = neither touched nor
-			//  pressed, 1 = both touched and pressed. So no need to track 
-			//  anything other than the isPressed boolean.
-
+			//  one was pressed.
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
 			//  isTouched: Duplicates isPressed.
 			//  isPressed: As expected.
@@ -748,9 +751,10 @@ THREE.VRController.supported = {
 			'grips',
 
 
+			//  MENU
 			//  The menu button is the tiny button above the thumbpad -- NOT
-			//  the one below it. Simple binary on / off press.
-
+			//  the one below it.
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
 			//  isTouched: Duplicates isPressed.
 			//  isPressed: As expected.
@@ -776,7 +780,7 @@ THREE.VRController.supported = {
 		//  Previously I’d named the style “Rift” and referred to this as a 
 		// “Rift” in the comments because it’s so much easier to write and to 
 		//  say than “Oculus”. Lazy, right? But deep down in your dark heart 
-		//  I know you agree with me. But I changed it all to “oculus” now 
+		//  I know you agree with me. I’ve changed it all to “oculus” now 
 		//  because that’s what both the headset and the controllers report 
 		//  themselves as. There’s no mention of “Rift” in those ID strings at
 		//  all. I felt in the end consistency was better than ease.
@@ -784,8 +788,9 @@ THREE.VRController.supported = {
 		style: 'oculus',
 
 
+		//  THUMBSTICK
 		//  Oculus’s thumbstick has axes values and is also a button.
-		//  The Y-axis is Regular.
+		//  The Y-axis is “Regular”.
 		//
 		//              Top: Y = -1
 		//                   ↑
@@ -797,28 +802,27 @@ THREE.VRController.supported = {
 		buttons: [
 
 
+			//  THUMBSTICK
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
-			//  isTouched: ACTUAL BOOLEAN FOR TOUCH!
+			//  isTouched: YES has real touch detection.
 			//  isPressed: As expected.
 
 			'thumbstick',
 
 
-			//  ===  Chromium  ===============================================
-			//  Oculus’s trigger in Chromium is far more fire-happy than Vive’s.
-			//  Compare these thresholds to Vive’s trigger:
-			//  if( value >  0.00 ) isTouched = true else isTouched = false
-			//  UPON ENGAGING
-			//  if( value >= 0.10 ) isPressed = true
-			//  UPON RELEASING
-			//  if( value <  0.10 ) isPressed = false
-
-			//  ===  Firefox  ================================================
-			//  UPON ENGAGING
-			//  if( value >= 0.10 ) isTouched = isPressed = true
-			//  UPON RELEASING
-			//  if( value <  0.10 ) isTouched = isPressed = false
-
+			//  TRIGGER
+			//  Oculus’s trigger in Chromium is far more fire-happy than 
+			//  Vive’s. Compare these thresholds to Vive’s trigger. 
+			//
+			//  Chromium
+			//  if( value >  0.0 ) isTouched = true else isTouched = false
+			//  if( value >= 0.1 ) isPressed = true else isPressed = false
+			//
+			//  Firefox
+			//  if( value >= 0.1 ) isTouched = isPressed = true
+			//  if( value <  0.1 ) isTouched = isPressed = false
+			//  --------------------------------------------------------------
 			//  value:     Analog 0 to 1.
 			//  isTouched: Duplicates isPressed in FF, independent in Chrome.
 			//  isPressed: Corresponds to value.
@@ -826,26 +830,30 @@ THREE.VRController.supported = {
 			'trigger',
 
 
+			//  GRIP
 			//  Oculus’s grip button follows the exact same press thresholds
 			//  as its trigger.
 
 			'grip',
 
 
+			//  A B X Y
 			//  Oculus has two old-school video game buttons, A and B. (On the
 			//  left-hand controller these are X and Y.)
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
-			//  isTouched: ACTUAL BOOLEAN FOR TOUCH!
+			//  isTouched: YES has real touch detection.
 			//  isPressed: As expected.
 
 			'A', 'B',
 
 
+			//  THUMBREST
 			//  Oculus has an inert base “button” that’s really just a resting
-			//  place for your thumbs. It only reports a binary on/off for 
-			//  touch and has no press.
+			//  place for your thumb. It does NOT report press.
+			//  --------------------------------------------------------------
 			//  value:     Always 0.
-			//  isTouched: ACTUAL BOOLEAN FOR TOUCH!
+			//  isTouched: YES has real touch detection.
 			//  isPressed: N/A.
 
 			'thumbrest'
@@ -886,31 +894,38 @@ THREE.VRController.supported = {
 		// “Microsoft Windows Mixed Reality Spatial Motion Controller”?
 		//  Perhaps best to just label them as “Microsoft” for now and revisit
 		//  in the future if need be.
-		//  NOTE: Currently Windows MR devices only function in MS Edge!
+		//
+		//  NOTE: Currently Windows Mixed Reality devices only function in 
+		//  Microsoft Edge on latest builds of Windows 10.
 
 		style: 'microsoft',
-
-
-		//  Microsoft’s controllers have two 2D axes. Both Y-axes are Regular.
-		//
-		//              Top: Y = -1
-		//                   ↑
-		//    Left: X = -1 ←─┼─→ Right: X = +1
-		//                   ↓
-		//           Bottom: Y = +1
-
 		axes: [
 
 
-			//  The thumbstick is super twitchy, fires when you breathe on it.
-			//  And sometimes when you don’t. 
+			//  THUMBSTICK
+			//  The thumbstick is super twitchy, seems to fire quite a bit on
+			//  its own. Its Y-axis is “Regular”.
+			//
+			//              Top: Y = -1
+			//                   ↑
+			//    Left: X = -1 ←─┼─→ Right: X = +1
+			//                   ↓
+			//           Bottom: Y = +1
 
 			{ name: 'thumbstick', indexes: [ 0, 1 ]},
+
+
+			//  THUMBPAD
+			//  Operates exactly the same as the thumbstick but without the
+			//  extra twitchiness.
+
 			{ name: 'thumbpad',   indexes: [ 2, 3 ]}
 		],
 		buttons: [
 
 
+			//  THUMBSTICK
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
 			//  isTouched: Duplicates isPressed.
 			//  isPressed: As expected.
@@ -918,22 +933,23 @@ THREE.VRController.supported = {
 			'thumbstick',
 
 
-			//  Trigger’s physical range of motion noticably exceeds the range
-			//  of values reported. For example when engaging you can continue
-			//  to squueze beyond when the value reports 1.0. And when 
+			//  TRIGGER
+			//  Its physical range of motion noticably exceeds the range of
+			//  values reported. For example when engaging you can continue
+			//  to squueze beyond when the value reports 1. And when 
 			//  releasing you will reach value === 0 before the trigger is 
 			//  completely released. The value property dictates touch and
 			//  press states as follows:
-
-			//  UPON ENGAGING
-			//  if( value >= 0.00 && value < 0.10 ) NO values reported at all!
+			//
+			//  Upon engaging
+			//  if( value >= 0.00 && value < 0.10 ) NO VALUES REPORTED AT ALL!
 			//  if( value >= 0.10 ) isTouched = true
 			//  if( value >= 0.12 ) isPressed = true
-
-			//  UPON RELEASING
+			//
+			//  Upon releasing
 			//  if( value <  0.12 ) isPressed = false
 			//  if( value == 0.00 ) isTouched = false
-
+			//  --------------------------------------------------------------
 			//  value:     Analog 0 to 1.
 			//  isTouched: Simulated, corresponds to value.
 			//  isPressed: Corresponds to value.
@@ -941,6 +957,8 @@ THREE.VRController.supported = {
 			'trigger',
 
 
+			//  GRIP
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
 			//  isTouched: Duplicates isPressed.
 			//  isPressed: As expected.
@@ -948,6 +966,8 @@ THREE.VRController.supported = {
 			'grip',
 
 
+			//  MENU
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
 			//  isTouched: Duplicates isPressed.
 			//  isPressed: As expected.
@@ -955,9 +975,11 @@ THREE.VRController.supported = {
 			'menu',
 
 
+			//  THUMBPAD
 			//  This is the only button that has actual touch detection.
+			//  --------------------------------------------------------------
 			//  value:     Binary 0 or 1, duplicates isPressed.
-			//  isTouched: ACTUAL BOOLEAN FOR TOUCH!
+			//  isTouched: YES has real touch detection.
 			//  isPressed: As expected.
 
 			'thumbpad'
